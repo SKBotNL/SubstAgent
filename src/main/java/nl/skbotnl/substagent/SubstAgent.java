9ea058dc2dfe5893f5f56087af1e6460e7b07e48
Sven <68018116+SKBotNL@users.noreply.github.com>
2025-10-01 22:55:46 +0200

    Print loaded message after transformer has been added

--- src/main/java/nl/skbotnl/substagent/SubstAgent.java
+++ src/main/java/nl/skbotnl/substagent/SubstAgent.java
@@ -5,7 +5,7 @@ import java.lang.instrument.Instrumentation;
 public class SubstAgent {
     public static void premain(String agentArgs, Instrumentation inst) {
         String version = SubstAgent.class.getPackage().getImplementationVersion();
-        System.out.println("[SubstAgent] Loaded version " + version);
         inst.addTransformer(new BufferedReaderTransformer());
+        System.out.println("[SubstAgent] Loaded version " + version);
     }
 }
